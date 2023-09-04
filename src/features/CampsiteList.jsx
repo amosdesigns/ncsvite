a0beb2bd4df599c4f043d115d181d510652e6202
jerome amos <mail@amosdesigns.net>
2023-09-04 15:52:50 -0400

    campsitesSlice added with redux
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- src/features/CampsiteList.jsx
+++ src/features/CampsiteList.jsx
@@ -1,22 +1,26 @@
 import { Row, Col } from 'reactstrap';
 import CampsiteCard from './CampsiteCard';
-import { CAMPSITES } from '../app/shared/CAMPSITES';
+import { selectAllCampsites } from './campsites/campsitesSlice';
 
 const CampsiteList = () => {
+  const campsites = selectAllCampsites();
   return (
-  <Row className="ms-auto">
-    {CAMPSITES && CAMPSITES.map( ( { id, name, image, featured, elevation, description } ) =>
-     <Col md='5' className='m-4' key={id}>
-        <CampsiteCard 
-        name={name} 
-        image={image} 
-        featured={featured} 
-        elevation={elevation} 
-        description={description} 
-        />
-      </Col>
-    )}
-  </Row>
+    <Row className="ms-auto">
+      {campsites &&
+        campsites.map(
+          ({ id, name, image, featured, elevation, description }) => (
+            <Col md="5" className="m-4" key={id}>
+              <CampsiteCard
+                name={name}
+                image={image}
+                featured={featured}
+                elevation={elevation}
+                description={description}
+              />
+            </Col>
+          )
+        )}
+    </Row>
   );
 };
 
