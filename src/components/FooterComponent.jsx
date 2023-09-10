e9068274c1f8432378a3c36a527d1afbe765cf99
jerome amos <mail@amosdesigns.net>
2023-09-10 12:51:21 -0400

    removed unsed components and updated links and styles added new font
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -9,35 +9,51 @@ const Footer = () => {
         <Row>
           <Col>
             <small>This will be the site navigation!</small>
-            <p><small>&copy;{setYearStatement} Jerome Amos</small></p>
+            <p>
+              <small>&copy;{setYearStatement} Jerome Amos</small>
+            </p>
           </Col>
           <Col>
             <h5>Social</h5>
-            <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/">
+            <a
+              className="btn btn-social-icon btn-instagram"
+              href="https://www.instagram.com/Bfamcooking">
               <i className="fa fa-instagram" />
             </a>{' '}
-            <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/">
+            <a
+              className="btn btn-social-icon btn-facebook"
+              href="https://www.facebook.com/BFAMCOOKING/">
               <i className="fa fa-facebook" />
             </a>{' '}
-            <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/">
+            <a
+              className="btn btn-social-icon btn-twitter"
+              href="https://twitter.com/Bfamcooking">
               <i className="fa fa-twitter" />
             </a>{' '}
-            <a className="btn btn-social-icon btn-google" href="http://youtube.com/">
+            <a
+              className="btn btn-social-icon btn-google"
+              href="https://youtube.com/c/bfamcooking?sub_confirmation=1">
               <i className="fa fa-youtube" />
             </a>
           </Col>
           <Col>
-           <a href="mailto: mail@bfamcooking.com" className="btn btn-link" role="button">
-              mail@bfamcooking.com
-            </a>
-            <a
-              href="tel:+16314507452"
-              className="btn btn-link"
-              role="button"
-              aria-label="to call me click this link">
-              +1.631.450.7452
-            </a>
-           
+            <Row>
+              <a
+                href="mailto: mail@bfamcooking.com"
+                className="btn btn-link"
+                role="button">
+                mail@bfamcooking.com
+              </a>
+            </Row>
+            <Row>
+              <a
+                href="tel:+16314507452"
+                className="btn btn-link"
+                role="button"
+                aria-label="to call me click this link">
+                +1.631.450.7452
+              </a>
+            </Row>
           </Col>
         </Row>
       </Container>
--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,13 +1,20 @@
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import { Container, Navbar,Row,Col, NavbarBrand } from 'reactstrap';
 import NucampLogo from '../app/assets/img/logo.png';
 
 const Header = () => {
   return (
     <Navbar dark color="primary" sticky="top" expand="md">
       <Container>
-        <NavbarBrand href="/">
-          <img src={NucampLogo} alt="nucamp logo" />
-        </NavbarBrand>
+        <Row>
+          <Col>
+            <NavbarBrand href="/">
+              <img src={NucampLogo} alt="nucamp logo" />
+            </NavbarBrand>
+          </Col>
+          <Col>
+            <h1>NuCamp Vite + Jerome</h1>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
