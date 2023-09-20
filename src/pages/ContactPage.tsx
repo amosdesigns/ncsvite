d62e7e9165dd0452c95df34c0fed4e912c6985fd
jerome amos <mail@amosdesigns.net>
2023-09-19 20:56:36 -0400

    ContactPage
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,13 +1,44 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import SubHeader from '../components/SubHeader';
+import { Col, Container, Row } from 'reactstrap';
 
 const ContactPage = () => {
   return (
-    <div>
-      <SubHeader current="Contact" detail={false} />
-      contact page
-    </div>
+    <Container>
+      <SubHeader current="Contact Us" detail={false} />
+      <Row className="row-content align-items-center">
+        <Col sm="4">
+          <h5>Our Address</h5>
+          <address>
+            37 maple wing drive
+            <br />
+            Central Islip, NY 11722
+            <br />
+            U.S.A.
+          </address>
+        </Col>
+        <Col>
+          <a role="button" className="btn btn-link" href="tel:+15164507452">
+            +1 (516)450-7452
+          </a>
+          <br />
+          <a
+            role="button"
+            className="btn btn-link"
+            href="mailto:mail@bfamcooking.com">
+            mail@bfamcooking.com
+          </a>
+        </Col>
+      </Row>
+      <Row className="row-content">
+        <Col xs="12">
+          <h2>Send Us Your Feedback</h2>
+          <hr />
+        </Col>
+        <Col md="10">TBD: ContactForm</Col>
+      </Row>
+    </Container>
   );
 };
 
