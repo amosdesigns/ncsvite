2ae006a0ab004199d3998cb527eac82d8f02b64b
amosdesigns <mail@amosdesigns.net>
2023-10-03 21:06:31 -0400

    Async Campsites
    Signed-off-by: amosdesigns <mail@amosdesigns.net>

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import AboutPage from './pages/AboutPage';
@@ -10,6 +13,11 @@ import Footer from './components/FooterComponent';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch] );
+  
   return (
     <div className="App">
       <Header />
--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,10 +1,43 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
-const initialState = { campsitesArray: CAMPSITES };
+
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { baseUrl } from '../../app/shared/baseUrl';
+import { mapImagesURL } from '../../utils/mapImageURL';
+
+//import { CAMPSITES } from '../../app/shared/CAMPSITES';
+
+export const fetchCampsites = createAsyncThunk(
+  'campsite/fetchCampsites',
+  async () => {
+    const response = await fetch( baseUrl + 'campsites' );
+    if ( !response.ok ) {
+      return Promise.reject( `Unable to fetch, Status: ${response.status}` );
+    }
+    const data = await response.json();
+    return data;
+  }
+);
+
+const initialState = { campsitesArray: [], isLoading:true, errMsg:'' };
+
 const campsitesSlice = createSlice({
   name: 'campsites',
-  initialState
-} );
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchCampsites.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchCampsites.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.errMsg = '';
+      state.campsitesArray = mapImagesURL(action.payload);
+    },
+    [fetchCampsites.rejected]: ( state, action ) => {
+      state.isLoading = false;
+      state.errMsg = action.error ? action.error.message : 'Fetch Failed :: Unable to fetch campsites';
+    }
+  }
+});
 
 export const campsitesReducer = campsitesSlice.reducer;
 
